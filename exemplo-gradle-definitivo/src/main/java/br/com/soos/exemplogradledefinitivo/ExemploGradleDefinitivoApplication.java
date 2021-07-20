8be3b8ac7f8b5ab2d3fce524f9424a0c96eaab81
Akisimaru <44277751+Akisimaru@users.noreply.github.com>
2021-07-20 20:16:08 -0300

    Add files via upload

---
+++ exemplo-gradle-definitivo/src/main/java/br/com/soos/exemplogradledefinitivo/ExemploGradleDefinitivoApplication.java
@@ -0,0 +1,13 @@
+package br.com.soos.exemplogradledefinitivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploGradleDefinitivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploGradleDefinitivoApplication.class, args);
+	}
+
+}
